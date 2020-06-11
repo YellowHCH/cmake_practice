a7eaf2744b8b195aeae2f0ce0fe83bb71972d6f0
yellow <2944297709@qq.com>
2020-06-11 14:43:27 +0800

    some note && some daily test

--- note/more_effective_c++.cpp
+++ note/more_effective_c++.cpp
@@ -48,11 +48,44 @@ void test_new()
   std::cout << std::endl;
   delete ptr;
 }
+#include <list>
+void test_stdcontainer_clear()
+{
+  std::cout << __func__ << std::endl;
+  std::list<int *> m_list;
+  int *val1 = new int(999);
+  int *val2 = new int(666);
+  m_list.push_back(std::move(val1));
+  m_list.push_back(val2);
+  m_list.clear();
+  std::cout << " *val1 ==  " << *val1 <<std::endl;
+}
 
-
+/// size of long int
+#include <string.h> // strlen
+static const char pathname[] = "/home/qspace/data/goodseed/";
+const char filename[] = "goodseed_9020061112.log";
+void printTypeSize()
+{
+  std::cout << __func__ << std::endl;
+  std::cout << "long int -- " << sizeof(long int) << std::endl;
+  std::cout << "long long -- " <<sizeof(long long) << std::endl;
+  std::cout << "double -- " << sizeof(double) << std::endl;
+  std::cout << "int -- " << sizeof(int) << std::endl;
+  std::cout << "size of str == " << sizeof(pathname) <<std::endl;
+  std::cout << "strlen == " << strlen(pathname) << std::endl;
+  char *file = const_cast<char*>(filename);
+  file += 9;
+  std::string strFile(file, 10);
+  long int iTime = std::stol(strFile);
+  std::cout << "strFile -> " <<strFile << std::endl;
+  std::cout << "iTime -> " <<iTime << std::endl;
+}
 
 int main()
 {
-  test_new();
+  //test_new();
+  //test_stdcontainer_clear();
+  printTypeSize();
   return 0;
 }
