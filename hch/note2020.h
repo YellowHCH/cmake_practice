cfda8d11a1ad980d902a8af8b80dee92b3c8c8a7
YellowHCH <2944297709@qq.com>
2020-11-07 17:11:08 +0800

    update: add vundle install menthod

--- hch/note2020.h
+++ hch/note2020.h
@@ -5,3 +5,12 @@
  * 3. git add . && git commit -m'xxx'
 */
 
+/*
+ * dev environment for arm linux
+ * sudo apt-get install gcc-arm-none-eabi
+ */
+
+/*
+ * git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+ * */
