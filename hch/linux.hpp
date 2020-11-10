996b939e11ea17a460180bf3dcb539351b32aaa2
YellowHCH <2944297709@qq.com>
2020-11-10 18:05:40 +0800

    note: add linux note

---
+++ hch/linux.hpp
@@ -0,0 +1,25 @@
+/*
+ * kernel src code tree
+ * arch                 ->      特定体系结构源码
+ * block                ->      块设备I/O层
+ * crypto               ->      加密API
+ * Documentation        ->      内核源码文档
+ * drivers              ->      设备驱动程序
+ * firmware             ->      使用某些驱动程序而需要的设备固件
+ * fs                   ->      vfs等文件系统
+ * include              ->      内核头文件
+ * init                 ->      内核引导和初始化
+ * ipc                  ->      进程间通信
+ * kernel               ->      核心子系统（调度程序）
+ * lib                  ->      通用内核函数
+ * mm                   ->      内存管理子系统和虚拟内存
+ * net                  ->      网络子系统
+ * samples              ->      实例示范代码
+ * scripts              ->      编译内核所需要的脚本
+ * security             ->      linux安全模块
+ * sound                ->      语音子系统
+ * usr                  ->      早期用户空间代码
+ * tools                ->      linux开发中的工具
+ * virt                 ->      虚拟化基础结构
+ * */
+
