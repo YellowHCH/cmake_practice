32957b024244f74ab646580dee9bfb754427016f
YellowHCH <2944297709@qq.com>
2022-05-10 14:45:50 +0800

    --tool cpp.vim

---
+++ hch/cpp-vim.sh
@@ -0,0 +1,4 @@
+git clone https://github.com/octol/vim-cpp-enhanced-highlight.git /tmp/vim-cpp-enhanced-highlight
+mkdir -p ~/.vim/after/syntax/
+mv /tmp/vim-cpp-enhanced-highlight/after/syntax/cpp.vim ~/.vim/after/syntax/cpp.vim
+rm -rf /tmp/vim-cpp-enhanced-highlight
